ce48027cbf82074a7c9dbc52ce73d9e697e685a9
christhechaiguy <71285865+christhechaiguy@users.noreply.github.com>
2020-11-27 13:21:55 -0500

    Update main.js

--- osa/build/assets/js/main.js
+++ osa/build/assets/js/main.js
@@ -16,9 +16,13 @@ $(function() {
         if ( $mobileMenuToggled == false ) {
             $('.navigation-container').addClass('toggled');
             $('.menu-toggle').addClass('toggled');
+
+            $mobileMenuToggled = true;
         } else {
             $('.navigation-container').removeClass('toggled');
             $('.menu-toggle').removeClass('toggled');
+            
+            $mobileMenuToggled = false;
         }
     });
 
