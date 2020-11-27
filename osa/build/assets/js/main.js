47e11ef4043d03ff7049dd6d42e49f36fd3a8b9a
christhechaiguy <71285865+christhechaiguy@users.noreply.github.com>
2020-11-27 00:09:00 -0500

    osa build

---
+++ osa/build/assets/js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    //lottie declaration
+
+    lottie.loadAnimation({
+        container: document.getElementById('osa-splash'), // the dom element that will contain the animation
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: 'assets/lottie/osa-splash.json' // the path to the animation json
+    });
+
+    setTimeout(function() {
+        $('.splash-wrapper').css({'opacity':'0'});
+    }, 3000)
+
+
+
+
+
+    
+});
+
