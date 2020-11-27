4837ba2f60f730eef640067baebe234b3143c489
christhechaiguy <71285865+christhechaiguy@users.noreply.github.com>
2020-11-27 10:08:27 -0500

    OSA - SIde Panel Build

--- osa/build/assets/js/main.js
+++ osa/build/assets/js/main.js
@@ -1,10 +1,6 @@
-
-
-
 $(function() {
 
     //lottie declaration
-
     lottie.loadAnimation({
         container: document.getElementById('osa-splash'), // the dom element that will contain the animation
         renderer: 'svg',
@@ -13,15 +9,11 @@ $(function() {
         path: 'assets/lottie/osa-splash.json' // the path to the animation json
     });
 
-
-    
 });
 
 
 $( window ).on( "load", function() {
-
     setTimeout(function() {
         $('.splash-wrapper').css({'opacity':'0'});
     }, 3000)
-
 });
