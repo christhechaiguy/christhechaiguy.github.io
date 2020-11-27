b5037262083d3965089a210d7d27d645337a5d1f
christhechaiguy <71285865+christhechaiguy@users.noreply.github.com>
2020-11-27 13:21:01 -0500

    menu toggle

--- osa/build/assets/js/main.js
+++ osa/build/assets/js/main.js
@@ -15,8 +15,10 @@ $(function() {
     $('.menu-toggle').click(function() {
         if ( $mobileMenuToggled == false ) {
             $('.navigation-container').addClass('toggled');
+            $('.menu-toggle').addClass('toggled');
         } else {
             $('.navigation-container').removeClass('toggled');
+            $('.menu-toggle').removeClass('toggled');
         }
     });
 
